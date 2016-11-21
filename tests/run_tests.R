094cd66b9d3aa36bd6a7b16f3deda8ee0cc92157
Hana Sevcikova <hanas@uw.edu>
2016-11-21 13:17:40 -0800

    dependencies updated

--- tests/run_tests.R
+++ tests/run_tests.R
@@ -1,15 +1,17 @@
 library(bayesPop)
 source('test_functions.R')
 
+CRAN <- TRUE
 warn <- options('warn')
 options(warn=2)
 test.expressions()
 
-# longer tests (comment out for submission)
- # test.prediction()
- # test.prediction.with.prob.migration()
- # test.expressions.with.VE(map=FALSE)
- # test.regional.aggregation()
- # test.life.table()
- 
+if(!CRAN) {
+# longer tests 
+	test.prediction()
+	test.prediction.with.prob.migration()
+	test.expressions.with.VE(map=FALSE)
+	test.regional.aggregation()
+	test.life.table()
+}
 options(warn=warn$warn)
