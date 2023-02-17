50d77b40b9b5cd1004994f641f2f20700782ae12
Hana Sevcikova <hanas@uw.edu>
2023-02-17 12:19:35 -0800

    fixed the MLTbx dataset; added data-raw

---
+++ data-raw/convert_to_rda.R
@@ -0,0 +1,21 @@
+# convert ASCII into rda to be used in the bayesPop package
+
+library(data.table)
+e <- new.env()
+
+dir <- "."
+datasets <- c("MLTbx", paste0("vwBaseYear", c(2010, 2012, 2015, 2017, 2019, 2022)))
+#datasets <- "vwBaseYear2019"
+datasets <- "MLTbx"
+
+for(f in datasets){
+    e[[f]] <- as.data.frame(fread(file.path(dir, paste0(f, ".txt"))))
+    if(f == "MLTbx") {
+        rown <- e[[f]][,1]
+        e[[f]] <- e[[f]][, -1]
+        rownames(e[[f]]) <- rown
+        colnames(e[[f]]) <- NULL
+    }
+    save(list = f, envir = e, file = paste0(f, ".rda"), compress = "bzip2")
+}
+
